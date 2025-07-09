2ca8f3c294ec7878012facc7a1f1274a178f19f7
Anita Hrabia <34211073+Hrabianka@users.noreply.github.com>
2025-07-09 19:19:49 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/goit-react-hw-01-components/',
   plugins: [react({
     jsxRuntime: 'automatic',
   })],
